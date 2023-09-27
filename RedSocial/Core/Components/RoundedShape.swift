1f2e0aaa673ff1f790e3590b8a802ed468385bff
Danielbzg <daniel.bozagarcia@gmail.com>
2023-09-27 21:58:53 +0200

    Login View created with components

---
+++ RedSocial/Core/Components/RoundedShape.swift
@@ -0,0 +1,18 @@
+//
+//  RoundedShape.swift
+//  RedSocial
+//
+//  Created by Daniel Boza García on 27/9/23.
+//
+
+import SwiftUI
+
+struct RoundedShape: Shape {
+    var corners: UIRectCorner
+
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: 80, height: 80))
+
+        return Path(path.cgPath )
+    }
+}
--- RedSocial/RedSocialApp.swift
+++ RedSocial/RedSocialApp.swift
@@ -12,7 +12,8 @@ struct RedSocialApp: App {
     var body: some Scene {
         WindowGroup {
             NavigationView{
-                ContentView()
+                //ContentView()
+                LoginView()
             }
         }
     }
